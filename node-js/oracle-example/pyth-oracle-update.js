2b28f287faeecd20ef4bccaea1606434594b9507
Josh <thisisjoshford@gmail.com>
2024-11-11 15:38:11 -0800

    fix: standardize price IDs across files & update comments

--- node-js/oracle-example/pyth-oracle-update.js
+++ node-js/oracle-example/pyth-oracle-update.js
@@ -7,29 +7,34 @@ const { functionCall } = require('@near-js/client');
 const sender = 'your-account.testnet';
 const receiver = 'pyth-oracle.testnet';
 
-const priceIds = [
-  // You can find the ids of prices at https://www.pyth.network/developers/price-feed-ids#near-testnet
+const PRICE_IDS = [
+  // Price ids can be found at https://www.pyth.network/developers/price-feed-ids#near-testnet
   // NOTE: Ensure you are using NEAR specific price ids & remove the '0x' prefix before using them
   'f9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b', // BTC/USD price id
   'ca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6', // ETH/USD price id
 ];
 
-async function getData(priceId, publishTime) {
+async function getHermesPriceData(priceId, publishTime) {
   try {
     // NOTE: For NEAR `testnet` use hermes-beta.pyth.network
-    const response = await axios.get(`https://hermes-beta.pyth.network/api/get_vaa?id=${priceId}&publish_time=${publishTime}`);
+    const response = await axios.get(
+      `https://hermes-beta.pyth.network/api/get_vaa?id=${priceId}&publish_time=${publishTime}`
+    );
     // NEAR requires data in hex format
-    const base64toHex = Buffer.from(response.data.vaa, "base64").toString("hex");
+    const base64toHex = Buffer.from(response.data.vaa, 'base64').toString('hex');
     return base64toHex;
   } catch (error) {
-    console.error('Error:', error.response ? error.response.data : error.message);
+    console.error(
+      'Error:',
+      error.response ? error.response.data : error.message
+    );
   }
 }
-async function updatePriceFeeds() {
+async function updatePythContractPriceFeeds() {
   const { rpcProvider, signer } = nearConnect(sender, 'testnet');
 
-  const publishTime = Math.floor(Date.now() / 1000)-1;
-  const data = await getData(priceIds[0], publishTime);
+  const publishTime = Math.floor(Date.now() / 1000) - 1;
+  const data = await getHermesPriceData(PRICE_IDS[0], publishTime);
 
   const result = await functionCall({
     sender,
@@ -41,9 +46,9 @@ async function updatePriceFeeds() {
   });
 
   console.log(
-    `Transaction -> https://testnet.nearblocks.io/txns/${result.outcome.transaction.hash}`
+    `Transaction 👉 https://testnet.nearblocks.io/txns/${result.outcome.transaction.hash}`
   );
   return result;
 }
 
-updatePriceFeeds();
+updatePythContractPriceFeeds();
--- node-js/oracle-example/pyth-oracle-view.js
+++ node-js/oracle-example/pyth-oracle-view.js
@@ -1,7 +1,13 @@
+// near-js import
+// https://www.npmjs.com/package/@near-js/client
 const { getTestnetRpcProvider, view } = require('@near-js/client');
 
-const BTC_PRICE_ID = 'f9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b';
-const ETH_PRICE_ID = 'ca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6';
+const PRICE_IDS = [
+  // Price ids can be found at https://www.pyth.network/developers/price-feed-ids#near-testnet
+  // NOTE: Ensure you are using NEAR specific price ids & remove the '0x' prefix before using them
+  'f9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b', // BTC/USD price id
+  'ca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6', // ETH/USD price id
+];
 
 async function getPrice(price_ID, symbol) {
   try {
@@ -18,5 +24,4 @@ async function getPrice(price_ID, symbol) {
   }
 }
 
-getPrice(BTC_PRICE_ID, 'BTC');
-getPrice(ETH_PRICE_ID, 'ETH');
+getPrice(PRICE_IDS[0], 'BTC/USD:');
